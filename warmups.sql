2117b192e0fda87efede24987d51bfaf6328d60a
Jerry Gao <gao@blackwright.com>
2017-04-12 17:37:44 -0400

    finished one warmup

--- warmups.sql
+++ warmups.sql
@@ -1,7 +1,7 @@
 -- Get a list of all users in California
 SELECT users.*
 FROM users
-JOIN states ON users.state_id = states.id 
+JOIN states ON users.state_id = states.id
 where states.name = 'California';
 
 -- Get a list of all airports in Kadeton
@@ -13,21 +13,36 @@ where cities.name = 'Kadeton';
 -- Get a list of all payment methods used on itineraries by the user with email address 'senger.krystel@marvin.io'
 SELECT DISTINCT payment_method
 FROM itineraries
-JOIN users ON itineraries.user_id = users.id 
+JOIN users ON itineraries.user_id = users.id
 WHERE email = 'senger.krystel@marvin.io';
 
 -- Get a list of prices of all flights whose origins are in Kochfurt Probably International Airport.
 SELECT price
 FROM flights
-JOIN airports ON flights.origin_id = airports.id 
+JOIN airports ON flights.origin_id = airports.id
 WHERE long_name = 'Kochfurt Probably International Airport';
 
 -- Find a list of all Airport names and codes which connect to the airport coded LYT.
-SELECT long_name, code 
-FROM airports
-JOIN flights ON (flights.origin_id = airports.id OR flights.destination_id = airports.id)
-ORDER BY long_name
-LIMIT 100;
-
+SELECT DISTINCT origin_name AS name, connections.origin_code AS code
+FROM
+(SELECT origins.long_name AS origin_name, origins.code AS origin_code, destinations.long_name AS destination_name, destinations.code
+FROM flights
+JOIN airports AS origins
+ON flights.origin_id = origins.id
+JOIN airports AS destinations
+ON flights.destination_id = destinations.id
+WHERE origins.code = 'LYT' OR destinations.code = 'LYT') AS connections
+WHERE connections.origin_code <> 'LYT'
+UNION
+SELECT DISTINCT destination_name, connections.destination_code
+FROM
+(SELECT origins.long_name, origins.code, destinations.long_name AS destination_name, destinations.code AS destination_code
+FROM flights
+JOIN airports AS origins
+ON flights.origin_id = origins.id
+JOIN airports AS destinations
+ON flights.destination_id = destinations.id
+WHERE origins.code = 'LYT' OR destinations.code = 'LYT') AS connections
+WHERE connections.destination_code <> 'LYT';
 
 -- Get a list of all airports visited by user Krystel Senger after January 1, 2012. (Hint, see if you can get a list of all ticket IDs first).
