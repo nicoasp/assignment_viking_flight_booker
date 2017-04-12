13872cfcfa18c44553a33216fba2b3fb428b1753
Jerry Gao <gao@blackwright.com>
2017-04-12 18:29:51 -0400

    finish intermediate queries

--- intermediate.sql
+++ intermediate.sql
@@ -20,8 +20,37 @@ LIMIT 1;
 
 -- Find the average flight distance for every city in California
 
-
+SELECT cities.name, ROUND(AVG(flights.distance), 2)
+FROM cities
+JOIN airports ON cities.id = airports.city_id
+JOIN flights ON flights.destination_id = airports.id
+JOIN states ON airports.state_id = states.id
+WHERE states.name = 'California'
+GROUP BY cities.name;
 
 -- Find the 3 users who spent the most money on flights in 2013
+
+SELECT users.*, SUM(flights.price) AS money_spent
+FROM users
+JOIN itineraries ON users.id = itineraries.user_id
+JOIN tickets ON tickets.itinerary_id = itineraries.id
+JOIN flights ON tickets.flight_id = flights.id
+GROUP BY users.id
+ORDER BY money_spent DESC
+LIMIT 3;
+
 -- Count all flights to OR from the city of Smithshire that did not land in Delaware
+
+SELECT COUNT(*)
+FROM flights
+JOIN airports AS origins ON flights.origin_id = origins.id
+JOIN airports AS destinations ON flights.destination_id = destinations.id
+JOIN cities AS origin_cities ON origins.city_id = origin_cities.id
+JOIN cities AS destination_cities ON destinations.city_id = destination_cities.id
+JOIN states AS destination_states ON destinations.state_id = destination_states.id
+WHERE 'Smithshire' IN (origin_cities.name, destination_cities.name)
+AND destination_states.name <> 'Delaware';
+
 -- Return the range of lengths of flights in the system(the maximum, and the minimum).
+SELECT MAX(flights.distance), MIN(flights.distance)
+FROM flights;
